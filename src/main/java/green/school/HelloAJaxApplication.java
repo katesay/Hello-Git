4fe1512c8a1cb929c62421fb12c957c6fdfc885a
Administrator <Administrator@SC-202106161522>
2022-11-02 16:56:09 +0900

    init commit

---
+++ src/main/java/green/school/HelloAJaxApplication.java
@@ -0,0 +1,13 @@
+package green.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAJaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAJaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/green/school/HelloAJaxApplicationTests.java
@@ -0,0 +1,13 @@
+package green.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAJaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
